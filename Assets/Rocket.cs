afc05a3550ace7dbd89c3557ed70131605d66148
kessris <kessris@hotmail.com>
2021-02-13 20:23:29 -0800

    collisions

--- Assets/Rocket.cs
+++ Assets/Rocket.cs
@@ -6,28 +6,70 @@ using UnityEngine;
 public class Rocket : MonoBehaviour
 {
     Rigidbody rigidBody;
+    AudioSource audioSource;
 
+    [SerializeField] float rcsThrust = 100f; // allows editing from Inspector but not outside this script
+    [SerializeField] float mainThrust = 100f;
 
     // Start is called before the first frame update
     void Start()
     {
         rigidBody = GetComponent<Rigidbody>();
+        audioSource = GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        ProcessInput();
+        Rotate();
+        Thrust();
     }
 
-    private void ProcessInput()
+    private void OnCollisionEnter(Collision collision)
+    {
+        switch(collision.gameObject.tag)
+        {
+            case "Friendly":
+                print("OK");
+                break;
+            default:
+                print("dead");
+                break;
+        }
+    }
+
+    private void Rotate()
+    {
+        rigidBody.freezeRotation = true; // take manual control of rotation
+        
+        if (Input.GetKey(KeyCode.A))
+        {
+            float rotationThisFrame = rcsThrust * Time.deltaTime;
+            transform.Rotate(Vector3.forward * rotationThisFrame);
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            float rotationThisFrame = rcsThrust * Time.deltaTime;
+            transform.Rotate(-Vector3.forward * rotationThisFrame);
+        }
+            
+        rigidBody.freezeRotation = false; // resume physics control of rotation
+    }
+
+    private void Thrust()
     {
         if (Input.GetKey(KeyCode.Space))
-            rigidBody.AddRelativeForce(Vector3.up);
-        if (Input.GetKey(KeyCode.A))
-            transform.Rotate(Vector3.forward);
-        else if (Input.GetKey(KeyCode.D))
-            transform.Rotate(-Vector3.forward);
+        {
+            rigidBody.AddRelativeForce(Vector3.up * mainThrust);
+            if (!audioSource.isPlaying)
+            {
+                audioSource.Play();
+            }
 
+        }
+        else
+        {
+            audioSource.Stop();
+        }
     }
 }
